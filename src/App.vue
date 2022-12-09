28060c522c8941bb6911c024aa91522197428781
ChoiByeol <honey333888@naver.com>
2022-12-09 20:30:10 +0900

    feat: vue 초기 세팅

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Hello />
+</template>
+
+<script>
+import Hello from "~/components/Hello";
+
+export default {
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      msg: "Hello Vue!",
+    };
+  },
+};
+</script>
